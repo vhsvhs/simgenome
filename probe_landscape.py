53ffc970c81ad4a35dd18dc7069b7a183afa4f32
Victor Hanson-Smith <victorhansonsmith@gmail.com>
2012-08-30 14:08:15 -0700

    I added probe_landscape.py

---
+++ probe_landscape.py
@@ -0,0 +1,35 @@
+############################
+#
+# probe_landscape.py
+#
+# INPUT: 
+# - a pickled population
+# - a fitness landscape
+# - an individual in the population (presumably the best-fit individual)
+#
+# METHOD:
+# - for each site in each cis sequence, mutate the site the other possible states,
+#     and then recalculate (i) the individual's fitness, (ii) if there was a network
+#    re-wiring event
+#
+# OUTPUT:
+# - overall stats:
+#    (a) proportion of 1-hop mutations that increase/decrease/unaffect fitness
+#    (b) proportion of 1-hop mutations that rewire the network
+#    (c) the intersection of data in (a) and (b)
+#
+# - a big table listing:
+#    site, x->y, fitness change, rewiring change
+#
+# - (maybe) an graphic showing rewiring hotspots.
+#
+############################
+
+
+from argparser import *
+from configuration import *
+from genetic_algorithm import *
+from population import *
+from version import *
+from debug_tools import *
+from cli import *
