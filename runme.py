e5313027191884538609bea9e22c2d736bb381b7
Victor Hanson-Smith <victorhansonsmith@gmail.com>
2012-09-03 13:17:46 -0700

    I removed references to debug_tools.py

--- runme.py
+++ runme.py
@@ -3,7 +3,6 @@ from configuration import *
 from genetic_algorithm import *
 from population import *
 from version import *
-from debug_tools import *
 from cli import *
 
 def check_workspace(ap):
--- test_include.py
+++ test_include.py
@@ -3,4 +3,3 @@ from configuration import *
 from genetic_algorithm import *
 from population import *
 from version import *
-from debug_tools import *
