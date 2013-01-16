28c63f3fa6239db86c1dc11e740946ef174958d8
vhsvhs <victorhansonsmith@gmail.com>
2013-01-15 16:27:08 -0800

    More work on plot_population.py

---
+++ KO.py
@@ -0,0 +1,106 @@
+#####################################################
+#
+# KO.py is derived from genetic_algorithmp.py (GA)
+# Whereas the GA module parallelizes over individuals,
+# the KO module parallelizes over genes.
+#
+####################################################
+
+from configuration import *
+from landscape import *
+from population import *
+from plots import *
+from logs import *
+
+class KO:        
+    genome = None
+    landscape = None    
+        
+    def __init__(self, ap):
+        id = ap.params["runid"] 
+    
+    def runko(self, ap):
+        """This is the main method of the KO module"""
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        if rank == 0:
+            self.runko_master(comm, ap)
+        else:
+            self.runko_slave(rank, comm, ap)
+
+    def get_fitness_stats(self, gid_fitness):
+        """gid_fitness is a hashtable, key = genome.id, value = fitness for that genome."""
+        f_vals = gid_fitness.values()
+        max_f = max(f_vals)
+        min_f = min(f_vals)
+        mean_f = mean(f_vals)
+        median_f = mean(f_vals)
+        std_f = std(f_vals)
+        return [max_f, min_f, mean_f, median_f, std_f]
+
+    def runko_master(self, comm, ap):
+        rank = comm.Get_rank()
+        
+        print "\n. KO.py 44: Rank", rank, "enetered runko_master"
+        wt_gid_fitness = {} # key = gene, value = KO'd fitness
+        ko_gid_fitness = {} # key = gene, value = KO'd fitness
+
+        for slave in range(1, comm.Get_size()):
+            if ap.params["verbosity"] > 2:
+                print "\n. KO.py line 40 - Master is waiting for data from slave", slave, "of", comm.Get_size()
+            [their_wt_gid_fitness, their_ko_gid_fitness] = comm.recv(source=slave, tag=12)
+
+            #if ap.params["verbosity"] > 50:
+            #    print "\n. genetic_algorithm.py line 73 - Master received from slave", slave, ":\their_wt_gid_fitness:", their_wt_gid_fitness, "\their_ko_gid_fitness:", their_ko_gid_fitness
+            for gid in their_wt_gid_fitness.keys():
+                wt_gid_fitness[gid] = their_wt_gid_fitness[gid]
+            for gid in their_ko_gid_fitness.keys():
+                ko_gid_fitness[gid] = their_ko_gid_fitness[gid]
+    
+        """Print LOGS/koX.txt with the fitness effect of each KO'd gene for genome X."""          
+        fout = open(ap.params["workspace"] + "/" + ap.params["runid"] + "/LOGS/ko.gen" + ap.params["generation"].__str__() + ".id" + ap.params["kogenome"].__str__() + ".txt", "w")
+        fout.write("Gene\tWT f\tKO f\tWT-KO\n")
+        gids = ko_gid_fitness.keys()
+        gids.sort()
+        for gid in gids:
+            line = gid.__str__()
+            line += "\t%.3f"%(wt_gid_fitness[gid])
+            line += "\t%.3f"%(ko_gid_fitness[gid])
+            line += "\t%.3f"%(wt_gid_fitness[gid]-ko_gid_fitness[gid]) 
+            line += "\n"
+            fout.write(line)
+        fout.close()
+
+
+    def runko_slave(self, rank, comm, ap):                                
+        """gid_fitness[gene ID] = [fitness at generation i]"""
+        wt_gid_fitness = {}
+        ko_gid_fitness = {}
+        
+        """Find my_items, which is a list of genes that this slave is responsible for."""
+        my_items = list_my_items(ap.params["rangetrs"], rank)
+        print "\n. KO.py 82: Rank", rank, "has these items:", my_items
+            
+        for gid in my_items:
+            """Calculate WT fitness for gene gid."""
+            wt_gid_fitness[ gid ] = self.landscape.get_fitness( self.genome, ap)
+            if ap.params["verbosity"] >= 2:
+                """and then plot the expression of all genes in this genome"""
+                title = "expr.wt.gen" + ap.params["generation"].__str__() + ".gene" + gid.__str__() 
+                filenameseed = ap.params["workspace"] + "/" + ap.params["runid"] + "/" + EXPR_PLOTS + "/" + title
+                write_expression_cran( self.genome, filenameseed, title, "time", "expression", ap)
+            
+            """Calculate KO fitness for gene gid."""
+            ko_gid_fitness[ gid ] = self.landscape.get_fitness( self.genome, ap, ko=[gid])
+            if ap.params["verbosity"] >= 2:
+                """and then plot the expression of all genes in this genome"""
+                title = "expr.ko.gen" + ap.params["generation"].__str__() + ".gene" + gid.__str__() 
+                filenameseed = ap.params["workspace"] + "/" + ap.params["runid"] + "/" + EXPR_PLOTS + "/" + title
+                write_expression_cran( self.genome, filenameseed, title, "time", "expression", ap)
+    
+        #print "\n. KO.94: slave", rank,"sending to master."
+        """Send data to master."""
+        comm.send( [wt_gid_fitness, ko_gid_fitness ], dest=0, tag=12)  
+        
+        
+        
---
+++ plot_population.py
@@ -0,0 +1,215 @@
+#
+# plot_population.py
+#
+#
+
+
+import os, re, sys
+from argparser import *
+ap = ArgParser(sys.argv)
+logdir = ap.getArg("--logdir")
+output_filename_seed = logdir + "/popgraph"
+
+#title = "Fitness (" + output_filename_seed + ")"
+title = "Population Fitness"
+xlab = "generations"
+ylab = "fitness"
+
+#
+# Read LOG/generations.txt
+#
+def build_plot_basics():
+    maxx = None
+    maxy = None
+    genstring = ""
+    genstring += "g <-c("
+    maxfstring = "maxf <-c("
+    minfstring = "minf <-c("
+    meanfstring = "meanf <-c("
+
+    fin = open(logdir + "/generations.txt", "r")
+    lines = fin.readlines()
+    fin.close()
+    
+    count_gen = 0
+    for l in lines:
+        if l.__len__() > 1:
+            tokens = l.split()
+            gen = int(tokens[1])
+            if maxx < gen:
+                maxx = gen
+            maxf = float(tokens[3])
+            if maxy < maxf:
+                maxy = maxf
+            minf = float(tokens[5])
+            meanf = float(tokens[7])
+            
+            genstring += gen.__str__() + ","   
+            maxfstring += maxf.__str__() + ","
+            minfstring += minf.__str__() + ","
+            meanfstring += meanf.__str__() + ","
+            count_gen += 1
+    
+    genstring = re.sub(",$", "", genstring)
+    genstring += ");\n"
+    maxfstring = re.sub(",$", "", maxfstring)
+    maxfstring += ");\n"
+    minfstring = re.sub(",$", "", minfstring)
+    minfstring += ");\n"
+    meanfstring = re.sub(",$", "", meanfstring)
+    meanfstring += ");\n"
+    
+    plotstring = "plot(c(0.0," + maxx.__str__() + "), c(0," + maxy.__str__() + "), type='n', main='" + title + "', xlab='" + xlab + "', ylab='" + ylab + "');\n"
+    
+    pointsstring = "points(g,maxf,type='l', col='black', lwd=1);\n"
+    pointsstring += "points(g,minf,type='l', col='black', lwd=1);\n"
+    pointsstring += "points(g,meanf,type='l', col='black', lwd=3);\n"
+    
+    r = genstring + "\n"
+    r += maxfstring + "\n"
+    r += minfstring + "\n"
+    r += meanfstring + "\n"
+    r += plotstring + "\n"
+    r += pointsstring + "\n"
+    return [r, count_gen]
+
+def build_poptrace(ngen):
+    [paths,marks] = build_coalescent_paths(ngen)
+    #print paths
+    return plot_coalescent_paths(paths,marks)
+
+def build_coalescent_paths(ngen):
+    paths = [] # array of [ (x1,y1), (x2,y2) ] lists
+    marks = [] # array of [ (x,y), char ]
+    
+    parent_fitness = {}
+    for gen in range(0, ngen):
+        my_parents = {}
+        children = []
+
+        # put values in my_parents
+        had_babies = []
+        mpath = None
+        if gen > 0:
+            mpath = logdir + "/mating.gen" + (gen - 1).__str__() + ".txt"
+            for line in open(mpath, "r").readlines():
+                if line.__contains__("+"):
+                    tokens = line.split()
+                    child = tokens[3]
+                    #children.append(child)
+                    p1 = tokens[5]
+                    if tokens.__len__() > 7:
+                        p2 = tokens[7]
+                    else:
+                        p2 = p1
+                    my_parents[child] = (p1,p2)
+                    if p1 not in had_babies:
+                        had_babies.append(p1)
+                    if p2 not in had_babies:
+                        had_babies.append(p2)
+                        
+
+        child_fitness = {}
+        fpath = logdir + "/gen" + gen.__str__() + ".txt"
+        for line in open(fpath, "r").readlines()[1:]:
+            tokens = line.split()
+            id = tokens[0]
+            f = float(tokens[1])
+            #print id, f
+            child_fitness[id] = f
+            children.append(id)
+        
+        if gen == 0:
+            for child in children:
+                my_parents[child] = (child,child)   
+                
+        #print "109:", gen    
+        #print "110:", children
+        #print "111:", child_fitness
+        #print "112:", my_parents
+                    
+        if gen > 0:  
+            for child in children:
+                # add lines
+                x1 = gen-1
+                #print my_parents[child][0]
+                y1 = parent_fitness[ my_parents[child][0] ]
+                
+                x2 = gen-1
+                y2 = parent_fitness[ my_parents[child][1] ]
+                
+                x3 = gen
+                y3 = child_fitness[child]
+
+                paths.append( [(x1,y1),(x3,y3)] )
+                paths.append( [(x2,y2),(x3,y3)] )
+
+                if child not in had_babies:
+                    if my_parents[child][0] == my_parents[child][1]: # clonal:
+                        marks.append( [(gen-1, parent_fitness[child]), "w"] )
+                    else:
+                        marks.append( [(gen-1, parent_fitness[child]), "x"] )
+                else:
+                    if my_parents[child][0] == my_parents[child][1]: # clonal:
+                        marks.append( [(gen-1, parent_fitness[child]), "c"] )
+                    else:
+                        marks.append( [(gen-1, parent_fitness[child]), "+"] )
+
+        for child in children:
+            parent_fitness[child] = child_fitness[child]
+            
+    return [paths, marks]         
+
+def plot_coalescent_paths(paths, marks):
+    ret = ""
+    for path in paths:
+        print path
+        s =  "x <-c(" + path[0][0].__str__() + "," + path[1][0].__str__() + ");\n"
+        s += "y <-c(" + path[0][1].__str__() + "," + path[1][1].__str__() + ");\n"
+        
+        if path[0][1] > path[1][1]:
+            color = "indianred1"
+            lwd = 0.05
+        else:
+            color = "royalblue1"  
+            lwd = 0.15
+        s += "points(x,y,type='l',col='" + color + "',lwd=" + lwd.__str__() + ");\n"
+        ret += s
+    for mark in marks:
+        s = "points(c(" + mark[0][0].__str__() + "), c(" + mark[0][1].__str__() + "),"
+        if mark[1] == "x":
+            s +=" type='p',pch=4,col='red3',cex=0.35,ps=5,font=1);\n" #sexual death
+        elif mark[1] == "w":
+            s +=" type='p',pch=6,col='red1',cex=0.35,ps=5,font=1);\n" #clonal death
+        elif mark[1] == "+":
+            s +=" type='p',pch=1,col='royalblue3',cex=0.35,ps=5,font=1);\n" # mating
+        else:
+            s +=" type='p',pch=2,col='deepskyblue',cex=0.35,ps=5,font=1);\n" # clonal
+        ret += s
+    return ret
+
+def get_legend():
+    l = "legend(\"bottomright\", c('clonal birth', 'sexual birth', 'clonal death', 'sexual death'), col=c('deepskyblue','royalblue3','red1', 'red3'), pch=c(2,1,6,4), cex=0.6);\n"
+    return l
+
+def execute_cran_string(cstring):    
+    fout_cran = open(output_filename_seed + ".cran", "w")
+    fout_cran.write("pdf('" + output_filename_seed + ".pdf', width=6, height=5);\n")
+    fout_cran.write(cstring)
+    fout_cran.write("dev.off();\n")
+    fout_cran.close()
+    os.system("r --no-save < " + output_filename_seed + ".cran")
+
+
+############################################
+#
+# main. . .
+#
+[s,ngen] = build_plot_basics()
+p = build_poptrace(ngen)
+l = get_legend()
+final = s + "\n" + p + "\n" + l
+execute_cran_string(final)
+
+
+
